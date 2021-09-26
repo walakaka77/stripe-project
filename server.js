b695905114b6374d5d005be3b1a6f420811f48db
walakaka <ahmadshafik92@gmail.com>
2021-09-26 16:28:12 +0800

    included all stripe keys, included all iitems

---
+++ server.js
@@ -0,0 +1,17 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
+console.log(stripeSecretKey, stripePublicKey)
+
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+//app.use(express.json())
+app.use(express.static('public'))
+
+app.listen(3000)
