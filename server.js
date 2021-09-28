f22315ace15f3c091c232d824185be86a684dbfa
walakaka <ahmadshafik92@gmail.com>
2021-09-28 23:06:37 +0800

    Added get code snippet to server.js file

--- server.js
+++ server.js
@@ -15,4 +15,17 @@ app.set('view engine', 'ejs')
 //app.use(express.json())
 app.use(express.static('public'))
 
+app.get('/store', function(req, res) {
+  fs.readFile('items.json', function(error, data) {
+    if (error) {
+      res.status(500).end()
+    } else {
+      res.render('store.ejs', {
+        stripePublicKey: stripePublicKey,
+        items: JSON.parse(data)
+      })
+    }
+  })
+})
+
 app.listen(3000)
